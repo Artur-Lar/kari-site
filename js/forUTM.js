5090f755ad81b1b4fff43f562c7b602a44e52588
1988Artur <larionov.artur@mail.ru>
2024-07-24 17:09:28 +0300

    conected forUTM.js

--- js/forUTM.js
+++ js/forUTM.js
@@ -4,16 +4,22 @@ function getUTMParameter(name) {
 }
 
 const utmMedium = getUTMParameter("utm_medium");
+console.log("UTM Medium:", utmMedium);
 
 const dynamicImage = document.getElementById("dynamic-image");
+console.log("Dynamic Image Element:", dynamicImage);
 
 if (utmMedium) {
   const validValues = ["26", "kmv", "08", "05", "95", "06", "09", "15", "07"];
   if (validValues.includes(utmMedium)) {
-    dynamicImage.src = `${utmMedium}.jpg`;
+    const newSrc = `images/${utmMedium}.jpg`;
+    dynamicImage.src = newSrc;
+    console.log("Image source updated to:", newSrc);
   } else {
     dynamicImage.src = "default.jpg";
+    console.log("Invalid utm_medium value, set to default.jpg");
   }
 } else {
   dynamicImage.src = "default.jpg";
+  console.log("No utm_medium value found, set to default.jpg"); // Логирование установки по умолчанию
 }
